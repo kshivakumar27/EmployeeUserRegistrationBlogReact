1aec98575beed2da8d6e717734664af62f4c1589
Shiva Kumar K <shivakumar.k@ltts.com>
2022-04-22 10:58:52 +0530

    Updates

---
+++ precache-manifest.1438967efef25d54170b97351cacc961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5033bebf0fe662f8f76aaaf28e3c594a",
+    "url": "/EmployeeUserRegistrationBlogReact/index.html"
+  },
+  {
+    "revision": "4fec8fc08f8b755a23f6",
+    "url": "/EmployeeUserRegistrationBlogReact/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "438c0ddd771be4ceaa45",
+    "url": "/EmployeeUserRegistrationBlogReact/static/css/main.7a30bbaa.chunk.css"
+  },
+  {
+    "revision": "4fec8fc08f8b755a23f6",
+    "url": "/EmployeeUserRegistrationBlogReact/static/js/2.af4f9af0.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/EmployeeUserRegistrationBlogReact/static/js/2.af4f9af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438c0ddd771be4ceaa45",
+    "url": "/EmployeeUserRegistrationBlogReact/static/js/main.52331585.chunk.js"
+  },
+  {
+    "revision": "81bf7ccb1acdcedaf5e2",
+    "url": "/EmployeeUserRegistrationBlogReact/static/js/runtime-main.900ae218.js"
+  }
+]);
